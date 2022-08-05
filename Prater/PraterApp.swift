2a8918b15e3e862734d070aa1efbd96fc0d7d35b
Chris Searle <chris@chrissearle.org>
2022-08-05 11:37:40 +0200

    Initial Commit

---
+++ Prater/PraterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PraterApp.swift
+//  Prater
+//
+//  Created by Chris Searle on 05/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct PraterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
